63ccf0d7284cac3c559168b134edae691eacb7f5
vedansh pratap singh <vedanshpsingh@gmail.com>
2025-08-19 11:41:24 +0530

    Update VinylVisualizer.jsx

--- src/components/VinylVisualizer.jsx
+++ src/components/VinylVisualizer.jsx
@@ -5,8 +5,30 @@ import { Vinyl } from '../Vinyl'; // Make sure this path is correct
 
 function Scene({ isPlaying, albumArtUrl }) {
   const texture = useTexture(albumArtUrl || '/fallback-art.png');
-
   const modelRef = useRef();
+  
+  // --- NEW CODE FOR LOGGING ---
+  const controlsRef = useRef();
+  const { camera } = useThree(); // Get access to the R3F camera object
+
+  useEffect(() => {
+    const controls = controlsRef.current;
+    if (!controls) return;
+
+    // This function will run every time the camera moves
+    const logCameraPosition = () => {
+      console.log(
+        `Camera Position: x: ${camera.position.x.toFixed(2)}, y: ${camera.position.y.toFixed(2)}, z: ${camera.position.z.toFixed(2)}`
+      );
+    };
+
+    controls.addEventListener('change', logCameraPosition);
+
+    // Cleanup function to remove the event listener
+    return () => {
+      controls.removeEventListener('change', logCameraPosition);
+    };
+  }, [camera]);
 
   useFrame(() => {
     if (isPlaying && modelRef.current) {
@@ -21,7 +43,7 @@ function Scene({ isPlaying, albumArtUrl }) {
 
       <Vinyl modelRef={modelRef} albumTexture={texture} />
       
-      <OrbitControls enableZoom={false} />
+      <OrbitControls enableZoom={true} />
     </>
   );
 }
